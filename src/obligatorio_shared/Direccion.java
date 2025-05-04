0e837733d9f0e40b4a933297516320eabf39a2d4
MtsPiedra <matipiedra10@gmail.com>
2025-05-04 12:12:11 -0300

    implemented "Direccion" class

--- src/obligatorio_shared/Direccion.java
+++ src/obligatorio_shared/Direccion.java
@@ -1,5 +1,16 @@
+/*
+ * Autores: [Matías Piedra 354007], [Joaquin Piedra ######]
+ */
 package obligatorio_shared;
 
-public class Direccion {
-    
+/**
+ * Representa las 6 direcciones posibles para una banda en el juego Triángulos.
+ */
+public enum Direccion {
+    NOROESTE, // Q  -  Se llama como Direccion.Noroeste
+    NORESTE,  // E  -  Se llama como Direccion.Noreste
+    ESTE,     // D  -  Se llama como Direccion.Este
+    SURESTE,  // C  -  Se llama como Direccion.Sureste
+    SUROESTE, // Z  -  Se llama como Direccion.Suroeste
+    OESTE     // A  -  Se llama como Direccion.Oeste
 }
